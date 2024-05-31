938ac33c1c3c444895c565d5f3a58d94d75f31ac
Andy Ron <randy.njfu@gmail.com>
2024-05-31 21:23:19 +0800

    封装请求函数

--- panda-shop/src/pages/my/my.vue
+++ panda-shop/src/pages/my/my.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
-import '@/utils/http'
+import { http } from '@/utils/http'
 
 const memberStore = useMemberStore()
 
 // 测试请求
-const getData = () => {
-  uni.request({
+const getData = async () => {
+  const res = await http<number[]>({
     method: 'GET',
     url: '/home/banner',
+    header: {},
   })
+  console.log('请求成功', res.result)
 }
 </script>
 
